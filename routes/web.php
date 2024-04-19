5eb9a7d50d6d10eda2620158134443555e37c371
T1Chon <100689490+T1Chon@users.noreply.github.com>
2024-04-19 15:32:38 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+ 
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+ 
+Route::view('/', 'welcome');
+ 
+Route::get('posts', [PostController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+->name('posts'); 
+ 
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+ 
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+ 
+require __DIR__.'/auth.php';
